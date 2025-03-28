0ffc675a42d845c2e56815752c86074af6a1e5a0
GaheeKIM <gaheekim610@gmail.com>
2025-03-29 01:41:21 +1000

    REMS-88 Redirect page depends on user's login status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,9 +11,12 @@ import Profile from "./pages/Profile";
 import Property from "./pages/Property";
 import { useState } from "react";
 
-function App() {
-  const [role, setRole] = useState("");
+import { useAuth } from "./context/AuthContext";
 
+function App() {
+  const [role, setRole] = useState("individual");
+
+  const { user } = useAuth();
   const handleRole = (value) => {
     setRole(value);
   };
@@ -17,10 +25,47 @@ function App() {
     <Router>
       <Navbar handleRole={handleRole} />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={user ? <Navigate to="/property" replace /> : <Login />}
+        />
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate to="/property" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/property" replace /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={
+            user ? (
+              <Navigate to="/property" replace />
+            ) : (
+              <Register role={role} />
+            )
+          }
+        />
+        <Route
+          path="/profile"
+          element={user ? <Profile /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/property"
+          element={user ? <Property /> : <Navigate to="/login" replace />}
+        />
+        {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register role={role} />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/property" element={<Property />} />
+        <Route path="/property" element={<Property />} /> */}
       </Routes>
     </Router>
   );

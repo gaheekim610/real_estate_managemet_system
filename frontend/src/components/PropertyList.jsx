d5b8205c08dc16bedb3b918f7dbbb99607fda8b2
GaheeKIM <gaheekim610@gmail.com>
2025-03-28 23:32:14 +1000

    git commit -m "REMS-60 #done Develop property list component"

--- frontend/src/components/PropertyList.jsx
+++ frontend/src/components/PropertyList.jsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/AuthContext";
 import axiosInstance from "../axiosConfig";
+import PropertyCard from "./PropertyCard";
 
 const PropertyList = ({ properties, setProperties, setEditingProperty }) => {
   const { user } = useAuth();
@@ -21,31 +22,13 @@ const PropertyList = ({ properties, setProperties, setEditingProperty }) => {
     <div>
       {Array.isArray(properties) ? (
         properties.map((property) => (
-          <div
-            key={property._id}
-            className="bg-gray-100 p-4 mb-4 rounded shadow"
-          >
-            <h2 className="font-bold" name="title">
-              {property.title}
-            </h2>
-            <p name="description">{property.description}</p>
-            <p name="image" className="text-sm text-gray-500">
-              {property.image}
-            </p>
-            <div className="mt-2">
-              <button
-                onClick={() => setEditingProperty(property)}
-                className="mr-2 bg-yellow-500 text-white px-4 py-2 rounded"
-              >
-                Edit
-              </button>
-              <button
-                onClick={() => handleDelete(property._id)}
-                className="bg-red-500 text-white px-4 py-2 rounded"
-              >
-                Delete
-              </button>
-            </div>
+          <div className="m-3">
+            <PropertyCard
+              key={property._id}
+              property={property}
+              setEditingProperty={setEditingProperty}
+              onDelete={handleDelete}
+            />
           </div>
         ))
       ) : (

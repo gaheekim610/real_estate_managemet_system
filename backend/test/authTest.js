38475d6c564f52b863ed3ec185dc9001e13e7db9
GaheeKIM <gaheekim610@gmail.com>
2025-03-30 12:10:04 +1000

    REMS-89 login unit test and add JWT token test

--- backend/test/authTest.js
+++ backend/test/authTest.js
@@ -1,7 +1,8 @@
 const chai = require("chai"); // Assertion library used for writing test cases
 const sinon = require("sinon"); // Library used for mocking/stubbing functions
 const mongoose = require("mongoose"); // To generate mock ObejctId value
-const { registerUser } = require("../controllers/authController");
+const bcrypt = require("bcrypt");
+const { registerUser, loginUser } = require("../controllers/authController");
 const { expect } = chai;
 // Import User
 const User = require("../models/User");
@@ -10,6 +11,47 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 describe("Auth Controller - Create Individual User(Signup)", () => {
+  it("should return JWT token and user role when signup", async () => {
+    const req = {
+      body: {
+        name: "Test general user",
+        email: "test@test.com",
+        password: "Test1111",
+        role: "individual",
+      },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    };
+
+    const _id = new mongoose.Types.ObjectId();
+    const createdUser = {
+      _id,
+      id: _id.toString(),
+      ...req.body,
+    };
+
+    const findOneStub = sinon.stub(User, "findOne").resolves(null);
+    const createStub = sinon.stub(User, "create").resolves(createdUser);
+    const tokenStub = sinon.stub(jwt, "sign").returns("mocked-token");
+
+    await registerUser(req, res);
+
+    const result = res.json.getCall(0).args[0];
+
+    expect(res.status.calledWith(201)).to.be.true;
+    expect(result).to.have.property("token", "mocked-token");
+    expect(result).to.have.property("role", "individual");
+    expect(result).to.have.property("email", req.body.email);
+
+    // cleanup
+    findOneStub.restore();
+    createStub.restore();
+    tokenStub.restore();
+  });
+
   it("should create a user successfully", async () => {
     // Stub JWT token
     const tokenStub = sinon.stub(jwt, "sign").returns("mocked-token");
@@ -129,5 +171,70 @@ describe("Auth Controller - Create Individual User(Signup)", () => {
 
     findOneStub.restore();
     createStub.restore();
+  });
+});
+
+describe("Auth Controller - Login", async () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should login and return token successfully", async () => {
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "Test1111",
+      },
+    };
+
+    const testUser = {
+      _id: new mongoose.Types.ObjectId(),
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10),
+      name: "Test User",
+      role: "agent",
+    };
+
+    sinon.stub(User, "findOne").resolves(testUser);
+    sinon.stub(bcrypt, "compare").resolves(true);
+    sinon.stub(jwt, "sign").returns("mocked-token");
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await loginUser(req, res);
+
+    expect(res.status.calledWith(200)).to.be.true;
+    expect(res.json.calledOnce).to.be.true;
+    const responseData = res.json.getCall(0).args[0];
+    expect(responseData).to.have.property("token", "mocked-token");
+  });
+
+  it("should fail with wrong credentials", async () => {
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "Test1111",
+      },
+    };
+
+    sinon.stub(User, "findOne").resolves(null);
+    sinon.stub(bcrypt, "compare").resolves(true);
+    sinon.stub(jwt, "sign").returns("mocked-token");
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    await loginUser(req, res);
+
+    console.log("wrong credentials", res.status.getCall(0).args[0]);
+
+    expect(res.status.calledWith(400)).to.be.true;
+    expect(res.json.calledWithMatch({ message: "Invalid email or password" }))
+      .to.be.true;
   });
 });
